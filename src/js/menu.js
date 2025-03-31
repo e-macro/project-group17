e4980c57d77ae183687e576c0da86bae3e586d3d
Volodymyr Buhlak <suedanny2@yahoo.com>
2025-03-31 18:58:26 +0300

    fixed ouryachts button (?) and fixed mobile anchor links

--- src/js/menu.js
+++ src/js/menu.js
@@ -5,11 +5,17 @@
     // Додати атрибут data-modal-close на кнопку закриття
     closeModalBtn: document.querySelector('[data-menu-close]'),
     // Додати атрибут data-modal на бекдроп модалки
+    closeModalBtn2: document.querySelector('[data-menu-close2]'),
+    closeModalBtn3: document.querySelector('[data-menu-close3]'),
+    closeModalBtn4: document.querySelector('[data-menu-close4]'),
     modal: document.querySelector('[data-menu]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn2.addEventListener('click', toggleModal);
+  refs.closeModalBtn3.addEventListener('click', toggleModal);
+  refs.closeModalBtn4.addEventListener('click', toggleModal);
 
   function toggleModal() {
     // is-open це клас який буде додаватися/забиратися на бекдроп при натисканні на кнопки
